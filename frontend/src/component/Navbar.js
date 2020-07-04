4377dce1099bff6ec54ce091aed758f44ddd7a57
bocahrokok <indieunistudy@gmail.com>
2020-07-04 22:21:29 +0700

    card done; navbar logout

--- frontend/src/component/Navbar.js
+++ frontend/src/component/Navbar.js
@@ -1,20 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../App";
 
 const Navbar = () => {
+  const isAuth = useContext(AuthContext);
   return (
     <div>
       <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <svg
-            className="fill-current h-8 w-8 mr-2"
-            width="54"
-            height="54"
-            viewBox="0 0 54 54"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-          </svg>
           <span className="font-semibold text-xl tracking-tight">
             Course Review
           </span>
@@ -59,7 +52,7 @@ const Navbar = () => {
               href="#"
               className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
             >
-              <Link to="/login">Login</Link>
+              <Link to="/login">{isAuth ? "Logout" : "Login"}</Link>
             </a>
           </div>
         </div>
--- frontend/src/containers/DasboardList.js
+++ frontend/src/containers/DasboardList.js
@@ -2,9 +2,101 @@ import React from "react";
 
 const DasboardList = () => {
   return (
-    <div className="flex w-full">
-      <div className="container  max-w-md shadow-md rounded bg-white ">
-        test
+    <div className="flex w-full justify-around flex-wrap">
+      <div className="container max-w-sm bg-white shadow-lg rounded-lg overflow-hidden my-4 px-4 py-5">
+        <p className="py-2 text-lg text-gray-700">Intro to Computer Science</p>
+
+        <div className="flex items-center mt-4 text-gray">
+          <h1 className="text-md">Instructor : Prof. Pramuji</h1>
+        </div>
+        <div>
+          <h1 className="text-md">Faculty Mathematics</h1>
+        </div>
+
+        <div>
+          <h1 className="text-md">Semester</h1>
+        </div>
+        <div>
+          <h1 className="text-md">Year</h1>
+        </div>
+
+        <div>
+          <h1 className="text-md">Rating</h1>
+          <h3 className="text-gray-600">five star</h3>
+        </div>
+
+        <div>
+          <h5 className="text-md">Comment</h5>
+          <p class="text-gray-600 text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+
+      <div className="container max-w-sm bg-white shadow-lg rounded-lg overflow-hidden my-4 ml-10 px-4 py-5">
+        <p className="py-2 text-lg text-gray-700">Intro to Computer Science</p>
+
+        <div className="flex items-center mt-4 text-gray">
+          <h1 className="text-md">Instructor : Prof. Pramuji</h1>
+        </div>
+        <div>
+          <h1 className="text-md">Faculty Mathematics</h1>
+        </div>
+
+        <div>
+          <h1 className="text-md">Semester</h1>
+        </div>
+        <div>
+          <h1 className="text-md">Year</h1>
+        </div>
+
+        <div>
+          <h1 className="text-md">Rating</h1>
+          <h3 className="text-gray-600">five star</h3>
+        </div>
+
+        <div>
+          <h5 className="text-md">Comment</h5>
+          <p class="text-gray-600 text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+
+      <div className="container max-w-sm bg-white shadow-lg rounded-lg overflow-hidden my-4 ml-10 px-4 py-5">
+        <p className="py-2 text-lg text-gray-700">Intro to Computer Science</p>
+
+        <div className="flex items-center mt-4 text-gray">
+          <h1 className="text-md">Instructor : Prof. Pramuji</h1>
+        </div>
+        <div>
+          <h1 className="text-md">Faculty Mathematics</h1>
+        </div>
+
+        <div>
+          <h1 className="text-md">Semester</h1>
+        </div>
+        <div>
+          <h1 className="text-md">Year</h1>
+        </div>
+
+        <div>
+          <h1 className="text-md">Rating</h1>
+          <h3 className="text-gray-600">five star</h3>
+        </div>
+
+        <div>
+          <h5 className="text-md">Comment</h5>
+          <p class="text-gray-600 text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
       </div>
     </div>
   );
